176a70330c780f1eab5ff0da091465189c13dacd
jenkins-systeemgebruiker <noreply-jenkins@omgeving.vlaanderen.be>
2026-08-06 13:38:27 +0000

    poc: deploy from jenkins-Publiek-codelijst-rie-iepr-main-44

--- src/99_deploy_latest.js
+++
@@ -1,6 +0,0 @@
-import { deploy_latest } from '@milieuinfo/maven-metadata-generator-npm';
-import { set_env } from './utils/setenv.js';
-
-const omgeving = 'pr'
-set_env(omgeving)
-deploy_latest(omgeving)
